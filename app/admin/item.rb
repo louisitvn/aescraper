1a66e86180c87c632c4b26b30a1372556cf8fcb3
Louis Pham <louisitvn@gmail.com>
2014-07-26 07:23:41 +0700

    Remove NEW-ITEM button

--- app/admin/item.rb
+++ app/admin/item.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Item do
-
+  actions :all, :except => [:new]
   
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
